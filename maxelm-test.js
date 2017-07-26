2903af1959a584a7fd9ba90e7fd59491c3190cc1
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 15:56:52 +0800

    Updated test.

--- maxelm-test.js
+++ maxelm-test.js
@@ -2,12 +2,12 @@
 const assert = require( "assert" );
 const maxelm = require( "./maxelm.js" );
 
-assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ) > 4, true, "should be true" );
+assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ) > 4, true, "should be equal to true" );
 
-assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ) > 6, false, "should be false" );
+assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ) > 6, false, "should be equal to false" );
 
-assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ), 5, "should be '5'" );
+assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ), 5, "should be equal to 5" );
 
-assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ).compare( 7 ), true, "should be true" );
+assert.equal( maxelm( [ 1, 2, 3, 4, 5 ] ).compare( 7 ), true, "should be equal to true" );
 
 console.log( "ok" );
